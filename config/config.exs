834f38b31e4e9a15d01937cdf80bc314ec8b5aad
MikaAK <mika@kalathil.me>
2021-09-18 15:35:10 -0700

    chore: add default config

---
+++ config/config.exs
@@ -0,0 +1,12 @@
+import Config
+
+config :binance_api,
+  api_key: "<BINANCE_API_KEY>",
+  secret_key: "<BINANCE_SECRET_KEY>",
+  base_url: "https://api.binance.com",
+  secure_receive_window: 5_000,
+
+  request: [
+    pool_timeout: 5_000,
+    receive_timeout: 15_000
+  ]
