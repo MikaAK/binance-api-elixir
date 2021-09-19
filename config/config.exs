fb10ea2f56d903f467be8871eeb6dca0c492ff77
MikaAK <mika@kalathil.me>
2021-09-18 17:30:43 -0700

    chore: update config with futures url

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,7 @@ config :binance_api,
   api_key: "<BINANCE_API_KEY>",
   secret_key: "<BINANCE_SECRET_KEY>",
   base_url: "https://api.binance.com",
+  base_futures_url: "https://fapi.binance.com",
   secure_receive_window: 5_000,
 
   request: [
