a61fccd72dabd2175115701f71916818d8d97e1c
MikaAK <mika@kalathil.me>
2021-09-18 18:56:54 -0700

    fix: make sure defaults are set in config.ex

--- lib/binance_api/config.ex
+++ lib/binance_api/config.ex
@@ -3,22 +3,22 @@ defmodule BinanceApi.Config do
 
   @app :binance_api
 
-  def api_key, do: get_key(:api_key)
-  def secret_key, do: get_key(:secret_key)
-  def base_url, do: get_key(:base_url)
-  def base_futures_url, do: get_key(:base_futures_url)
-  def secure_receive_window, do: get_key(:secure_receive_window)
+  def api_key, do: get_key(:api_key, "<BINANCE_API_KEY>")
+  def secret_key, do: get_key(:secret_key, "<BINANCE_SECRET_KEY>")
+  def base_url, do: get_key(:base_url, "https://api.binance.com")
+  def base_futures_url, do: get_key(:base_futures_url, "https://fapi.binance.com")
+  def secure_receive_window, do: get_key(:secure_receive_window, 5_000)
 
-  def request_pool_timeout, do: get_sub_key(:request, :pool_timeout)
-  def request_receive_timeout, do: get_sub_key(:request, :receive_timeout)
+  def request_pool_timeout, do: get_sub_key(:request, :pool_timeout, 5_000)
+  def request_receive_timeout, do: get_sub_key(:request, :receive_timeout, 15_000)
 
-  def get_key(key), do: Application.get_env(@app, key)
+  def get_key(key, default), do: Application.get_env(@app, key, default)
 
-  def get_sub_key(key, sub_key) do
-    case get_key(key) do
-      nil -> raise "Cannot find config for #{key} in :binance_api, this is required"
+  def get_sub_key(key, sub_key, default) do
+    case get_key(key, default) do
+      nil -> default
       key_list when is_list(key_list) -> key_list[sub_key]
-      _ -> raise "Config for #{key} in :binance_api is not a keyword list"
+      _ -> default
     end
   end
 end
